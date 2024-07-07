d0eb1ba65cfdc8a560533ad11ba7606f968df047
zahoor456 <z1721375@gmail.com>
2024-07-07 17:34:46 +0530

    Completed

---
+++ script.js
@@ -0,0 +1,28 @@
+
+let defaultMode = "dark";
+let heroCaption = document.querySelectorAll('.hero-text');
+let color = document.querySelector(':root');
+let orderBtn = document.querySelector('.order-btn');
+function changeTextColor(tag, color){
+    tag.style.color = color;
+}
+function toggleMode(){
+    let btn = document.querySelector('#toggle-mode');
+    if(defaultMode == "dark"){
+        btn.innerHTML = "<i class=\"fa-solid fa-moon\"></i>";
+        defaultMode="light";
+        color.style.setProperty('--dark','#f7f7f7');
+        color.style.setProperty('--light','#e5bf4a');
+        color.style.setProperty('--golden','#070707');
+        heroCaption[0].style.color = "white";
+        orderBtn.style.border = "1px solid var(--dark)";
+    }
+    else{
+        btn.innerHTML = "<i class=\"fa-solid fa-sun\"></i>";
+        defaultMode = "dark";
+        color.style.setProperty('--dark','#070707');
+        color.style.setProperty('--light','#f7f7f7');
+        color.style.setProperty('--golden','#e5bf4a');
+        orderBtn.style.border = "1px solid var(--golden)";
+    }
+}
